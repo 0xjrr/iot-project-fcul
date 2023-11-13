d5e35ee0bcd9ddaf5df205c2a3a2465ca79e4ac9
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-11-13 13:16:08 +0000

    add fragment and progress circle

--- cloud_infra/frontend_web/src/app/components/Grid.jsx
+++ cloud_infra/frontend_web/src/app/components/Grid.jsx
@@ -1,30 +1,50 @@
-import { Grid, Col, Card, Text, Metric } from "@tremor/react";
+"use client";
+import {
+  Grid,
+  Col,
+  Card,
+  Text,
+  Metric,
+  Flex,
+  ProgressCircle,
+} from "@tremor/react";
 
 export default () => (
-  <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2">
-    <Col numColSpan={1} numColSpanLg={2}>
+  <>
+    <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-4">
+      <Col numColSpan={1} numColSpanLg={2}>
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 1</Metric>
+        </Card>
+      </Col>
       <Card>
         <Text>Title</Text>
-        <Metric>KPI 1</Metric>
+        <Metric>KPI 2</Metric>
       </Card>
-    </Col>
-    <Card>
-      <Text>Title</Text>
-      <Metric>KPI 2</Metric>
-    </Card>
-    <Col>
+      <Col>
+        <Card>
+          <Text>Title</Text>
+          <Metric>KPI 3</Metric>
+        </Card>
+      </Col>
       <Card>
         <Text>Title</Text>
-        <Metric>KPI 3</Metric>
+        <Metric>KPI 4</Metric>
       </Card>
-    </Col>
-    <Card>
-      <Text>Title</Text>
-      <Metric>KPI 4</Metric>
-    </Card>
-    <Card>
-      <Text>Title</Text>
-      <Metric>KPI 5</Metric>
-    </Card>
-  </Grid>
+      <Card>
+        <Text>Title</Text>
+        <Metric>KPI 5</Metric>
+      </Card>
+      <Card className="max-w-sm mx-auto">
+        <Flex className="space-x-5" justifyContent="start">
+          <ProgressCircle value={75.5} size="lg" />
+          <div>
+            <Text className="font-medium text-gray-700">Value Callout (75%)</Text>
+            <Text>Text context</Text>
+          </div>
+        </Flex>
+      </Card>
+    </Grid>
+  </>
 );
