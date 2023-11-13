ea59843269c1fb19a5846711778fb7c2a3b14b19
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-11-13 13:15:38 +0000

    add width contraint

--- cloud_infra/frontend_web/src/app/page.js
+++ cloud_infra/frontend_web/src/app/page.js
@@ -3,7 +3,7 @@ import Grid from './components/Grid'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 min-w-full">
       <h1 className=''>
         Dashboard Template:
       </h1>
