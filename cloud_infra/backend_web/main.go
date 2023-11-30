7b151066a2af6fbe13909ae4eb1429a20222ff70
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-11-30 14:44:15 +0000

    add middleware to handle OPTIONS request

--- cloud_infra/backend_web/main.go
+++ cloud_infra/backend_web/main.go
@@ -36,6 +36,22 @@ type SensorData struct {
 	Timestamp time.Time
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins, or specify
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
