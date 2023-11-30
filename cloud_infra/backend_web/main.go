ab74ce5120fa029a3c74baeb0beee37800a74d00
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-11-30 14:43:47 +0000

    db env variables defined in dockerfile and routes

--- cloud_infra/backend_web/main.go
+++ cloud_infra/backend_web/main.go
@@ -37,10 +37,17 @@ type SensorData struct {
 }
 
 func main() {
-	// Connect to the database
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	// Migrate the schema
@@ -48,6 +55,7 @@ func main() {
 
 	// Set up Gin
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 
 	r.GET("/", func(c *gin.Context) {
 
@@ -65,11 +73,32 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"message": "User created successfully", "user": user})
 	})
 
+	r.GET("/users", func(c *gin.Context) {
+		var users []User
+		result := db.Find(&users) // Query all users
+		if result.Error != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, users)
+	})
+
 	// Endpoint for fetching sensor data
 	r.GET("/data", func(c *gin.Context) {
 		var sensorData []SensorData
 		db.Find(&sensorData)
 		c.JSON(http.StatusOK, sensorData)
+	})
+
+	// Endpoint for inserting sensor data
+	r.POST("/data", func(ctx *gin.Context) {
+		var data SensorData
+		if err := ctx.ShouldBindJSON(&data); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db.Create(&data)
+		ctx.JSON(http.StatusOK, gin.H{"message": "User created successfully", "user": data})
 	})
 
 	// Run the server
