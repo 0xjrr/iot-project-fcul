f4d74cfa5be12a5967fce50f8799ac4b3a7f21e6
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-01 12:56:10 +0000

    change form to make it nicer

--- cloud_infra/frontend_web/src/app/components/FormCreateUser.jsx
+++ cloud_infra/frontend_web/src/app/components/FormCreateUser.jsx
@@ -1,18 +1,18 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 const FormCreateUser = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('');
-  const [device, setDevice] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [device, setDevice] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     const userData = {
       Name: name,
       Email: email,
@@ -22,39 +22,68 @@ const FormCreateUser = () => {
     };
 
     try {
-      const response = await axios.post('http://localhost:8080/user', userData);
-      console.log('User created:', response.data);
+      const response = await axios.post("http://localhost:8080/user", userData);
+      console.log("User created:", response.data);
       // Handle the response here, maybe clear the form or show a success message
     } catch (error) {
-      console.error('Error creating user:', error);
+      console.error("Error creating user:", error);
       console.log(userData);
       // Handle error here, show error message to user
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <form onSubmit={handleSubmit} className="space-y-4">
+      {" "}
+      {/* Add spacing between each form group */}
+      <div className="flex justify-between items-center">
+        <label className="w-1/3">Name:</label> {/* Adjust width as needed */}
+        <input
+          className="bg-transparent text-white rounded-md border-white border-2 w-2/3"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <div className="flex justify-between items-center">
+        <label className="w-1/3">Email:</label>
+        <input
+          className="bg-transparent text-white rounded-md border-white border-2 w-2/3"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
-      <div>
-        <label>Age:</label>
-        <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+      <div className="flex justify-between items-center">
+        <label className="w-1/3">Age:</label>
+        <input
+          className="bg-transparent text-white rounded-md border-white border-2 w-2/3"
+          type="number"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
       </div>
-      <div>
-        <label>Gender:</label>
-        <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
+      <div className="flex justify-between items-center">
+        <label className="w-1/3">Gender:</label>
+        <input
+          className="bg-transparent text-white rounded-md border-white border-2 w-2/3"
+          type="text"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        />
       </div>
-      <div>
-        <label>Device:</label>
-        <input type="text" value={device} onChange={(e) => setDevice(e.target.value)} />
+      <div className="flex justify-between items-center">
+        <label className="w-1/3">Device:</label>
+        <input
+          className="bg-transparent text-white rounded-md border-white border-2 w-2/3"
+          type="text"
+          value={device}
+          onChange={(e) => setDevice(e.target.value)}
+        />
       </div>
-      <button type="submit">Create User</button>
+      <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700">
+        Create User
+      </button>
     </form>
   );
 };
