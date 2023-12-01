7b989e9f877213eb68f2156aca9c8d10aba4dc37
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-01 12:56:58 +0000

    add copied component and to user selection spot

--- cloud_infra/frontend_web/src/app/page.js
+++ cloud_infra/frontend_web/src/app/page.js
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import DashboardGrid from './components/DashboardGrid'
-import FormCreateUser from './components/FormCreateUser'
+import Image from "next/image";
+import DashboardGrid from "./components/DashboardGrid";
+import FormCreateUser from "./components/FormCreateUser";
+import UserSelection from "./components/UserSelection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-w-full">
-      <h1 className=''>
-        Dashboard Template:
-      </h1>
+      <div className="flex justify-between w-4/5 mx-auto">
+        <h1>Dashboard</h1>
+        <UserSelection /> {/* This is your new component */}
+      </div>
       <DashboardGrid />
       <FormCreateUser />
     </main>
-  )
+  );
 }
