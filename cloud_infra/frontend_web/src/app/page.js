dcf51cad320b63c5d1107cbc3926a5214f84f008
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-01 21:33:04 +0000

    add img to the page

--- cloud_infra/frontend_web/src/app/page.js
+++ cloud_infra/frontend_web/src/app/page.js
@@ -2,16 +2,27 @@ import Image from "next/image";
 import DashboardGrid from "./components/DashboardGrid";
 import FormCreateUser from "./components/FormCreateUser";
 import UserSelection from "./components/UserSelection";
+import image from "./logo/logo.webp";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-w-full">
-      <div className="flex justify-between w-4/5 mx-auto">
-        <h1>Dashboard</h1>
+      <div className="flex justify-between w-full md:w-4/5 mx-auto">
+        <div className="flex items-center">
+          {" "}
+          <Image
+            src={image}
+            alt="Image Description"
+            height={100}
+          />
+          {" "}
+          <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl ">Dashboard</h1>
+        </div>
         <UserSelection /> {/* This is your new component */}
       </div>
       <DashboardGrid />
       <FormCreateUser />
+      <UserSelection />
     </main>
   );
 }
