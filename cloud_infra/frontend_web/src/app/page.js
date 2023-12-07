0d3e1e1e3fae4e4fd5bb3c9f56a87a7e7cbc12ef
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-07 15:46:33 +0000

    almost

--- cloud_infra/frontend_web/src/app/page.js
+++ cloud_infra/frontend_web/src/app/page.js
@@ -1,15 +1,43 @@
+"use client"
+
 import Image from "next/image";
 import DashboardGrid from "./components/DashboardGrid";
 import FormCreateUser from "./components/FormCreateUser";
 import UserSelection from "./components/UserSelection";
 import image from "./logo/logo.webp";
 import SensorDataComponent from "./components/SensorDataComponent";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [selectedUser, setSelectedUser] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Function to open the modal
+  const openModal = () => setIsModalOpen(true);
+  // Function to close the modal
+  const closeModal = () => setIsModalOpen(false);
+
+  useEffect(() => {
+    // Fetch users from the server
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/users"); // Replace with your API endpoint
+        const data = await response.json();
+        console.log(data);
+        setUsers(data); // Assuming the response data is an array of users
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
 
   const handleUserSelection = (user) => {
     setSelectedUser(user);
+    console.log(user); // Log the selected user ID or name here, for example: console.log(user.id);
   };
 
 
@@ -26,7 +54,7 @@ export default function Home() {
           {" "}
           <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl ">Dashboard</h1>
         </div>
-        <UserSelection onUserSelect={handleUserSelection}/>
+        <UserSelection onUserSelect={handleUserSelection} users={users} onOpenModal={openModal}/>
       </div>
       <DashboardGrid />
       <FormCreateUser />
