0523ba7cae6a418c8caf78f1b61978bfc6c48332
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-08 22:11:37 +0000

    publisher change for device

--- IdC-L03-TP-code/mqtt-pub.py
+++ IdC-L03-TP-code/mqtt-pub.py
@@ -1,9 +1,11 @@
 import paho.mqtt.client as mqtt 
 import time
-import random
+import datetime
+import json
 
 broker_hostname = "0.0.0.0"
 port = 8883 
+topic = "sensor/data"
 
 def on_connect(client, userdata, flags, return_code):
     if return_code == 0:
@@ -20,44 +22,37 @@ client.tls_insecure_set(True)
 client.connect(broker_hostname, port)
 client.loop_start()
 
+# Ask for device name input
+device_name = input("Enter the device name: ")
 
-topic = "your/topic"
-msg_count = 0
+# Function to process and publish data
+def publish_sensor_data(file_name, device_name):
+    with open(file_name, 'r') as file:
+        headers = file.readline().strip().split(';')
+        for line in file:
+            data_values = line.strip().split(';')
+            data_dict = dict(zip(headers, data_values))
+            data_dict["device_name"] = device_name
+            data_dict["timestamp"] = datetime.datetime.now().isoformat()
+            json_data = json.dumps(data_dict)
+            result = client.publish(topic, json_data)
+            status = result[0]
 
-# msg = []
-# msg.append('[{"model":"iris-KNN"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
-# msg.append('[{"model":"iris-GNB"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
-# msg.append('[{"model":"iris-SVC"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
-# msg.append('[{"model":"iris-DT"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
-# msg.append('[{"model":"iris-LR"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
-# msg.append('[{"model":"iris-LDA"},{"SepalLengthCm":5.9,"SepalWidthCm":3,"PetalLengthCm":5.1,"PetalWidthCm":1}]')
+            if status == 0:
+                print("Message "+ str(json_data) + " is published to topic " + topic)
+            else:
+                print("Failed to send message to topic " + topic)
 
+            time.sleep(1)  # Optional: sleep between messages
 
-try:
-    while True:
-        if msg_count == 0:
-            msg = []
-            models = ["iris-KNN", "iris-GNB", "iris-SVC", "iris-DT", "iris-LR", "iris-LDA"]
+# File name
+file_name = "online.csv"
 
-            for model in models:
-                element = {
-                    "model": model,
-                    "SepalLengthCm": round(random.uniform(4.0, 7.9), 1),
-                    "SepalWidthCm": round(random.uniform(2.0, 4.4), 1),
-                    "PetalLengthCm": round(random.uniform(1.0, 6.9), 1),
-                    "PetalWidthCm": round(random.uniform(0.1, 2.5), 1)
-                }
-                msg.append(str(element))
+# Publish data
+publish_sensor_data(file_name, device_name)
 
-        time.sleep(1)
-        result = client.publish(topic, msg[msg_count])
-        status = result[0]
-        if status == 0:
-            print("Message "+ str(msg[msg_count]) + " is published to topic " + topic)
-        else:
-            print("Failed to send message to topic " + topic)
-        msg_count = msg_count + 1
-        msg_count = msg_count % 5
+# Keep the script running
 
-finally:
-    client.loop_stop()
+print("Exiting...")
+client.disconnect()
+client.loop_stop()
