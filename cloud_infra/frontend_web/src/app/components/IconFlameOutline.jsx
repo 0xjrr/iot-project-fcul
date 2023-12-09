f7ad785c8e3bd548f687b02a7230a23929b8d14f
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-12-09 14:51:03 +0000

    add icons

---
+++ cloud_infra/frontend_web/src/app/components/IconFlameOutline.jsx
@@ -0,0 +1,33 @@
+// icon:flame-outline | Ionicons https://ionicons.com/ | Ionic Framework
+import * as React from "react";
+
+function IconFlameOutline(props) {
+  return (
+    <svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={32}
+        d="M112 320c0-93 124-165 96-272 66 0 192 96 192 272a144 144 0 01-288 0z"
+      />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={32}
+        d="M320 368c0 57.71-32 80-64 80s-64-22.29-64-80 40-86 32-128c42 0 96 70.29 96 128z"
+      />
+    </svg>
+  );
+}
+
+export default IconFlameOutline;
